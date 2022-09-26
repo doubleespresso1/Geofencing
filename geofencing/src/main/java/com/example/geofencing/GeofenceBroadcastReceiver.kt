a6d83802aa54bbfb598b0fde948a13363f10bf87
raqbiel <raqbiel@gmail.com>
2022-09-26 19:45:59 +0200

    Initial commit

---
+++ geofencing/src/main/java/com/example/geofencing/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,15 @@
+package com.example.geofencing
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class GeofenceBroadcastReceiver: BroadcastReceiver(){
+
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        TODO("Not yet implemented")
+    }
+
+}
+
+private const val TAG = "Geofence Receiver"
